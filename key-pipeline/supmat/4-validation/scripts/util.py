4932286bd5b3602069e4648a0c1d2af853d63ccb
Lokmane CHEBOUBA <lchebouba@gmail.com>
2019-07-23 10:41:12 +0200

    Update util.py

--- key-pipeline/supmat/4-validation/scripts/util.py
+++ key-pipeline/supmat/4-validation/scripts/util.py
@@ -1,7 +1,7 @@
 # General-purpose library for statistical validation
 # --------------------------------------------------
 # This file is part of the Supplementary Material of the submission entitled:
-# Hepatocellular carcinoma computational models identify key protein-complexes associated to tumor progression
+# A pipeline to create predictive models: application to the tumor progression of hepatocellular carcinoma
 # Authors: Maxime Folschette, Vincent Legagneux, Arnaud Poret, Lokmane Chebouba, Carito Guziolowski and Nathalie Théret
 
 
