4951797111b524b5b4cd31d650f5747f0e439128
Lokmane CHEBOUBA <lchebouba@gmail.com>
2019-07-23 10:35:17 +0200

    Update black_list_gen_prot.sh

--- key-pipeline/supmat/2-pathrider/scripts/black_list_gen_prot.sh
+++ key-pipeline/supmat/2-pathrider/scripts/black_list_gen_prot.sh
@@ -3,7 +3,7 @@
 # construct a list of genes and proteins from a list of nodes
 # ----------------------------------------------
 # This file is part of the Supplementary Material of the submission entitled:
-# Hepatocellular carcinoma computational models identify key protein-complexes associated to tumor progression
+# A pipeline to create predictive models: application to the tumor progression of hepatocellular carcinoma
 # Authors: Maxime Folschette, Vincent Legagneux, Arnaud Poret, Lokmane Chebouba, Carito Guziolowski and Nathalie Théret
 
 # Usage:
