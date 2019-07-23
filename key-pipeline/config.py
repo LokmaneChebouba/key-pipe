061cb80fc171f26de54e78dcbb5f3b24bd2dba7f
Lokmane CHEBOUBA <lchebouba@gmail.com>
2019-07-23 14:07:03 +0200

    Add files via upload

---
+++ key-pipeline/config.py
@@ -0,0 +1,10 @@
+import sys
+import os
+import subprocess
+
+
+os.system('wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh')
+os.system('bash Miniconda3-latest-Linux-x86_64.sh')
+os.system('conda env create -f environment.yml')
+
+
