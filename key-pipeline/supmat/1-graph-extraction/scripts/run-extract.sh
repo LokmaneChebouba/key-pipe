f3a6c284cd366dbf53650859d8ebda1a87a1f1e2
Lokmane CHEBOUBA <lchebouba@gmail.com>
2019-07-26 14:34:37 +0200

    Update run-extract.sh

--- key-pipeline/supmat/1-graph-extraction/scripts/run-extract.sh
+++ key-pipeline/supmat/1-graph-extraction/scripts/run-extract.sh
@@ -6,9 +6,12 @@
 # A pipeline to create predictive models: application to the tumor progression of hepatocellular carcinoma
 # Authors: Maxime Folschette, Vincent Legagneux, Arnaud Poret, Lokmane Chebouba, Carito Guziolowski and Nathalie Théret
 
-#create data files
+#create data folders
+rm -r ./supmat/2-pathrider/data
 mkdir ./supmat/2-pathrider/data
+rm -r ./supmat/3-iggy/data
 mkdir ./supmat/3-iggy/data
+rm -r ./supmat/4-validation/data
 mkdir ./supmat/4-validation/data
 
 echo "Reading $1.."
