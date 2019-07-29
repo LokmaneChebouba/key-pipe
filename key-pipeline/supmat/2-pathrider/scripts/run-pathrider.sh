90855e8e24b64367452813bafb36977a1ec39258
Lokmane CHEBOUBA <lchebouba@gmail.com>
2019-07-29 14:59:57 +0200

    Update run-pathrider.sh

--- key-pipeline/supmat/2-pathrider/scripts/run-pathrider.sh
+++ key-pipeline/supmat/2-pathrider/scripts/run-pathrider.sh
@@ -11,13 +11,13 @@
 
 echo "Reading $3.."
 #create black list with genes and proteins
-sh ./supmat/2-pathrider/scripts/black_list_gen_prot.sh $3
+sh ./supmat/2-pathrider/scripts/excluded_gen_prot.sh $3
 
 echo "Reading $1.."
 echo "Reading gene names.."
 echo "Running Pathrider..."
 #./supmat/2-pathrider/scripts/pathrider stream $1 $2 $3 #-blacklist ./supmat/0-diff-analysis/data/LIHC_primary_weakly_expressed_genes.txt
-go run ./supmat/2-pathrider/scripts/pathrider/pathrider.go stream -blacklist ./supmat/2-pathrider/data/black_list_gen_prot.txt -out ./supmat/2-pathrider/data/out_pathrider.sif $1 ./supmat/2-pathrider/data/column_name.txt $2 > ./supmat/2-pathrider/pathrider_out.out
+go run ./supmat/2-pathrider/scripts/pathrider/pathrider.go stream -blacklist ./supmat/2-pathrider/data/excluded_gen_prot.txt -out ./supmat/2-pathrider/data/out_pathrider.sif $1 ./supmat/2-pathrider/data/genes_name.txt $2 > ./supmat/2-pathrider/pathrider_out.out
 #filter the graph from a black list
 #sh ./supmat/2-pathrider/scripts/remove_complexes.sh ./supmat/2-pathrider/data/out_pathrider.sif
 grep -wvi -f $3 ./supmat/2-pathrider/data/out_pathrider.sif > ./supmat/2-pathrider/data/out-filtered.sif
